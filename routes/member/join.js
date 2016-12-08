159efa7244335b3bb8be9630f6783386398bd91f
luckeyboy7 <luckeyboy7@naver.com>
2016-12-08 17:37:30 +0900

    Db test

--- routes/member/join.js
+++ routes/member/join.js
@@ -4,4 +4,23 @@ module.exports = function(app,User){
 		res.render('./member/join.ejs', { title: app.title });
 		
 	});
+	
+	app.post('/member/join',function(req,res){
+		var user = new User();
+		user.userId = req.body.userId;
+		user.userPassWord = req.body.userPassWord;
+		user.userEmail = req.body.userEmail;
+		
+		user.save(function(err){
+			//오류
+			if(err){
+				console.error(err);
+				res.json({result:0});
+				return;
+			}
+			//성공
+			res.json({result:1});
+			
+		});
+	});
 };
